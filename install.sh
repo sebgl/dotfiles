1b474d2c8bfbe2500a5070b6f837aa1e98062ba6
sebgl <contact.sebgl@gmail.com>
2019-11-03 18:44:15 +0100

    Add kitty config

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ cp -f dotfiles/.[a-z]* ~/
 
 # Copy fish config
 cp -f fish/config.fish ~/.config/fish/config.fish
+
+# Kitty config
+cp -f kitty/kitty.conf ~/.config/kitty/kitty.conf
+cp -f kitty/theme.conf ~/.config/kitty/theme.conf
